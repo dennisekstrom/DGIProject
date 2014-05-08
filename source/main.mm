1053e7cc0812c6406430f392ec5ca521f2bd5514
Tobias Wikström <tob.wikstrom@gmail.com>
2014-05-08 14:39:45 +0200

    test

--- source/main.mm
+++ source/main.mm
@@ -477,6 +477,8 @@ void AppMain() {
 
         
         //setup two viewports and draw one frame
+        
+        glViewport(0, 0, SCREEN_SIZE.x, SCREEN_SIZE.y);
         Render();
 //        glViewport(SCREEN_SIZE.x/2, 0, SCREEN_SIZE.x/3, SCREEN_SIZE.y/3);
 //        Render();
--- source/rangeterrain.h
+++ source/rangeterrain.h
@@ -11,35 +11,96 @@
 //
 //#include <vector>
 //
-//enum ControlPointFunc {
-//    FUNC_LINEAR,
-//    FUNC_COS,
-//    FUNC_SIN
-//};
-//
-//struct ControlPoint {
-//    int x,
-//    int y,
-//    ControlPointFunc func
-//};
-//
-//class RangeTerrain {
-//// private variables
-//private:
-//    vector<ControlPoint> controlPoints;
-//    bool controlPointChanged;
-//    
-//// public variables
-//public:
-//    float hmap[1024][1024];
-//    
-//// public functions
-//public:
-//    void SetControlPoint(int x, int y, ControlPointFunc func);
-//    void SetControlPoint(ControlPoint cp);
-//    void GenerateHMapFromControlPoints();
-//    
-//    inline void ControlPointChanged() { return controlPointChanged; }
-//}
-//
-//#endif
+
+#ifndef DGIProject_rangeterrain_h
+#define DGIProject_rangeterrain_h
+
+#include <vector>
+#include <math.h>
+
+#define PI              3.14159265359
+#define X_INTERVAL      1024
+#define Y_INTERVAL      1024
+
+using namespace std;
+
+enum ControlPointFuncType {
+    FUNC_LINEAR,
+    FUNC_COS,
+    FUNC_SIN
+};
+
+struct ControlPoint {
+public:
+    int x;
+    int y;
+    float h;
+    float spread;
+    float (*liftfunc)(float, float, float);
+    
+    ControlPoint(int x, int y, float h, float spread, ControlPointFuncType functype) {
+        this->x = x;
+        this->y = y;
+        this->h = h;
+        this->spread = spread;
+        
+        switch (functype) {
+            case FUNC_LINEAR:
+                this->liftfunc = lift_linear;
+                break;
+                
+            case FUNC_COS:
+                this->liftfunc = lift_cos;
+                break;
+                
+            case FUNC_SIN:
+                this->liftfunc = lift_sin;
+                break;
+        }
+    }
+        
+    inline static float lift_linear(float h, float spread, float dist) {
+        return dist <= spread ? h * (1 - dist / spread) : 0;
+    }
+    
+    inline static float lift_cos(float h, float spread, float dist) {
+        return dist <= spread ? h * cos((dist * PI) / (spread * 2)) : 0;
+    }
+    
+    inline static float lift_sin(float h, float spread, float dist) {
+        return dist <= spread ? h * (1 - sin((dist * PI) / (spread * 2))) : 0;
+    }
+    
+    float lift(int x, int y) {
+        float dx = this->x - x;
+        float dy = this->y - y;
+        float dist = sqrt(dx * dx + dy * dy);
+        return this->liftfunc(this->h, this->spread, dist);
+    }
+};
+
+class RangeTerrain {
+// private variables
+private:
+    vector<ControlPoint> controlPoints;
+    bool controlPointChanged;
+    
+// public variables
+public:
+    float hmap[X_INTERVAL][Y_INTERVAL];
+    
+    
+// private functions
+private:
+
+    
+// public functions
+public:
+    void SetControlPoint(int x, int y, float h, float spread, ControlPointFuncType func);
+    void SetControlPoint(ControlPoint cp);
+    void GenerateHMapFromControlPoints();
+    
+    inline bool ControlPointChanged() { return controlPointChanged; }
+};
+
+#endif
